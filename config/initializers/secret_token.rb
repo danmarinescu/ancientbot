7b8d6ece1121db6fcbb2136c434d59673a2e910f
Dan Marinescu <dan.marinescu@fieldaware.com>
2011-11-09 18:31:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ancientbot::Application.config.secret_token = 'b24b58cd8bdfe17b8a3c91e9ae3ce16099f7416f6d681b6278d4b1d1415fccc9cd1bc9554bbf06e4840d74dbfe0af96dd4a53e0d9b262321c102ba1b42933891'
